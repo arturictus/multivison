7f95670720d2e4498bb858afee90243f687550f4
Artur Pañach <arturictus@gmail.com>
2015-03-31 23:56:20 +0200

    added stylus configuration

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
-var express = require('express');
+var express = require('express'),
+  stylus = require('stylus'),
+  logger = require('morgan')
+  bodyParser = require('body-parser');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
+
+// stylus compile
+function compile(str, path) {
+  return stylus(str).set('filename', path);
+}
+
+// views
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(stylus.middleware(
+  {
+    src: __dirname + '/public',
+    compile: compile
+  }
+));
+app.use(express.static(__dirname + '/public'));
+
+// wildcard route
 app.get('*', function(req, res){
   res.render('index');
 });
 
+// server
 var port = 3030;
 app.listen(port);
 console.log('listening on port' + port + '...');
