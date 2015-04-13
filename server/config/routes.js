82b0c3e36e799630a2feb1c0c3b0f9593693414d
Artur Pañach <arturictus@gmail.com>
2015-04-13 23:18:47 +0200

    Implementing Serverside Authorization

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,20 +1,31 @@
-var auth = require('./auth');
+var auth = require('./auth'),
+    mongoose = require('mongoose'),
+    User = mongoose.model('User');
 
 module.exports = function(app){
+
+  app.get('/api/users', auth.requiresApiLogin, function(req, res){
+    User.find({}).exec(function(err, collection){
+      res.send(collection);
+    });
+  });
   // partials routes
   app.get('/partials/*', function(req, res){
     res.render('../../public/app/' + req.params[0]);
   });
-  // wildcard route
-  app.get('*', function(req, res){
-    res.render('index', {
-      bootstrappedUser: req.user
-    });
-  });
+
 
   app.post('/login', auth.authenticate);
   app.post('/logout', function(req, res){
     req.logout();
     res.end();
+  });
+  // wildcard route
+  // carefull to keep this wildcard at the end of the function.
+  // if not it will execute before the specified resources
+  app.get('*', function(req, res){
+    res.render('index', {
+      bootstrappedUser: req.user
+    });
   });
 };
