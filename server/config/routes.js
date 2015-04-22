f6ad573753acc0a61fcf33a0f86b3fc66361f0b7
Artur Pañach <arturictus@gmail.com>
2015-04-22 22:47:02 +0200

    clean up

--- server/config/routes.js
+++ server/config/routes.js
@@ -11,13 +11,13 @@ module.exports = function(app){
   app.get('/partials/*', function(req, res){
     res.render('../../public/app/' + req.params[0]);
   });
-
-
+  // login
   app.post('/login', auth.authenticate);
   app.post('/logout', function(req, res){
     req.logout();
     res.end();
   });
+
   // wildcard route
   // carefull to keep this wildcard at the end of the function.
   // if not it will execute before the specified resources
